026bf6966cd46988b38a76091c701a5565e70bbe
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-07-06 10:22:24 -0500

    Inicio Component

---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css'],
+})
+export class InicioComponent implements OnInit {
+  edad = 25;
+  peso = 60;
+  altura = 200;
+  sexo = 'Masculino';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  cambiarAltura(event: any){
+     this.altura = event.target.value;
+  }
+
+  masculino(){
+    this.sexo = 'Masculino';
+  }
+
+  femenino(){
+    this.sexo = 'Femenino'
+  }
+
+  calcularIMC(){
+    this.router.navigate(['/resultado']);  
+  }
+}
